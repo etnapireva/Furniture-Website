eb54ad354d1f19a81ff6e3726ae299857b8edb9d
limonmorina <149487995+limonmorina@users.noreply.github.com>
2024-01-23 15:31:02 +0100

    modifikim

--- login.php
+++ login.php
@@ -67,6 +67,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["loginUsername"]) && is
 <html lang="en">
 <head>
     <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <title>Log in</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
